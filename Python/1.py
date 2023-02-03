a20a14e1b826e10ea30326de386d0a79a182e817
ImCodeKing <lizhaoya21@mails.tsinghua.edu.cn>
2023-02-03 22:40:46 +0800

    1

---
+++ Python/1.py
@@ -0,0 +1,9 @@
+Num = eval(input("请输入实数：\n"))
+Total = 0
+while Num != 0:
+    Total += Num
+    Num = eval(input("请输入实数：\n"))
+print(Total)
+
+
+
---
+++ 英语听说/Transcriber software/MSP/system/buildin filters/ffdshow/__readme.txt
@@ -0,0 +1,5 @@
+more infomation please visit
+ http://sites.google.com/site/fftryoutgreen/home
+
+ contact:  fftryoutGreen@gmail.com
+
